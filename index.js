7e0fcee91a2f78e68fd32275d06b818c1a9412b5
next-davidiglesias <david.iglesias.zavala.next@bbva.com>
2020-02-10 12:48:04 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // Import lit-html functions
 import {html, render} from 'lit-html';
 
-const items = [{srcset: '', src:''},
-{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/Defenders-of-the-moon-tee.webp 1x', src:'https://intrinsic-layout-guide.glitch.me/img/tees/Defenders-of-the-moon-tee.jpg'},
-{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/Statix-Boy-Black-Tee.webp 1x', src:'https://intrinsic-layout-guide.glitch.me/img/tees/Statix-Boy-Black-Tee.jpg'},
-{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/Discover-Paradise-Vintage-Black-Scoop.webp 1x', src:'https://intrinsic-layout-guide.glitch.me/img/tees/Discover-Paradise-Vintage-Black-Scoop.jpg'},
-{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/The-Archer-Tee-BLACK.webp 1x', src:'https://intrinsic-layout-guide.glitch.me/img/tees/The-Archer-Tee-BLACK.jpg'},
-{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/Strange-World-Black-Tee.webp 1x', src:'https://intrinsic-layout-guide.glitch.me/img/tees/Strange-World-Black-Tee.jpg'},
-{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/Alchemist-Returns-Tee-BLACK.webp 1x', src:'https://intrinsic-layout-guide.glitch.me/img/tees/Alchemist-Returns-Tee-BLACK.jpg'},
-{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/Shred-Seattle-Tee.webp 1x"', src:'https://intrinsic-layout-guide.glitch.me/img/tees/Shred-Seattle-Tee.jpg'},]
+const items = [['A', 'Aceyalone', 'Aesop Rock', 'Angel Haze', 'Atmosphere'],['B','Babbletron','Blackalicious','Bike For Three','Black Sheep','Blueprint'],['C','Cecil Otter','Chali 2na','Chance The Rapper','Common Market','Cool Calm Pete'],['D','Danger Doom','Darc Mind','Dem Atlas','Dessa']]
 
 
 const myTemplate = ()=> html`
-<section class="horizontal-snap">
-  ${items.map((item)=> html`<a><picture>
-    <source type="image/webp" srcset="${item.srcset}">
-    <img src="${item.src}" loading="lazy">
-  </picture></a>`)}
-</section>
-
+<div class="sticky-slide">
+  <dl>
+    ${items.map((item)=>html`
+       ${item.map((i, index)=>html`
+         ${index === 0 ? 
+         html`
+         <dt>${i}</dt>`:
+         html `
+         <dd>${i}</dd>
+         `}
+       `)}
+    `)}
+  </dl>
+</div>
 
 `;
 
